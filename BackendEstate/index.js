d6b6e2d6703599e54462c3c0cb167ecaed726fa0
Akreminho <akrem.soltanix@gmail.com>
2023-12-24 19:35:19 +0100

    connect to database

--- BackendEstate/index.js
+++ BackendEstate/index.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
 
-const app = express();
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('connected to MongoDB');
+}).catch((err) =>{
+    console.log(err);
+});
 
-app.listen(4000, () =>{
-    console.log('Server is running on port 4000!! ');
-}
-);
+
+const app=express()
+app.listen(4000,()=>{
+    console.log('server is running on port 4000')
+})
