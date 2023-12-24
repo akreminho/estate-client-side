ea31cdee627b320f74e02b2c955750c7b7f98993
Akreminho <akrem.soltanix@gmail.com>
2023-12-24 20:54:59 +0100

    create a test Api

--- BackendEstate/index.js
+++ BackendEstate/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -13,4 +14,6 @@ mongoose.connect(process.env.MONGO).then(() =>{
 const app=express()
 app.listen(4000,()=>{
     console.log('server is running on port 4000')
-})
+});
+
+app.use("/BackendEstate/user", userRouter)
